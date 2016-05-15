cfc9035fbeda3edfab809d7b109caf2d6f62fad7
pn222en <pn222en@student.lnu.se>
2016-05-16 00:11:07 +0200

    solved confirm-ending

--- confirm-ending/solution1.js
+++ confirm-ending/solution1.js
@@ -1,11 +1,15 @@
 // Check if a string (first argument, str) ends with the given target string (second argument, target).
+// https://www.freecodecamp.com/challenges/confirm-the-ending
 function confirmEnding(str, target) {
-  var stringArray1 = str.split(" ");
-  var strignArray2 = target.split(" ");
+  var stringArray1 = str.split(' ');
+  var stringArray2 = target.split(' ');
+  var lastWord1 = stringArray1[stringArray1.length - 1];
+  var lastWord2 = stringArray2[stringArray2.length - 1];
 
-  if(stringArray1[stringArray1.length - 1] === stringArray2[stringArray2.length - 1]) {
-    return true
+  if (lastWord1.substr(lastWord1.length - lastWord2.length) === lastWord2) {
+    return true;
   } else {
-    return false
+    return false;
   }
+
 }
