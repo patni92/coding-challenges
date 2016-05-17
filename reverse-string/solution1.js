3b6074411d66e3db9b218dee06679654c94cae51
Patrik Nilsson <pn222en@student.lnu.se>
2016-05-17 23:41:02 +0200

    added link and description for reverse-string

--- reverse-string/solution1.js
+++ reverse-string/solution1.js
@@ -1,3 +1,5 @@
+//Reverse the provided string.
+//https://www.freecodecamp.com/challenges/reverse-a-string
 function reverseString (string) {
   var stringReverse = "";
   for(var i = string.length; i >= 0; i -= 1) {
